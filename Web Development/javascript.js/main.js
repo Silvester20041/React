a58f6a2c6adcb96389a6c515e5197a8bf36c868a
Silvester S <108716503+Silvester20041@users.noreply.github.com>
2023-09-05 17:45:30 +0530

    Add files via upload

---
+++ Web Development/javascript.js/main.js
@@ -0,0 +1,33 @@
+//number
+//strings
+//arrays
+//object
+
+//variable "var"
+
+var no1 = 10;
+var no2 = 20;
+var Name = "h";
+//partial case
+var MyNameIs = "Tamil";
+
+//camel case
+var myName = "English";
+
+//arrays
+var no = ["Tiger", "lion", "Rabbit", 1];
+no.push("hi");
+
+//loops
+for(var i = 0; i < 5; i ++){
+    var i1 = 10 ;
+    while(i1<17)
+    {
+        console.log(i);
+        i++
+    }
+
+
+}
+
+alert(no.reverse());
